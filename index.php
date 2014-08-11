66fd00d038344c70943ba0dea17b3f767bc2b132
Chris Rogers <cdrogers@iastate.edu>
2014-08-11 13:31:18 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+  <head>
+	<link rel="images/favicon.ico" />
+    <title>Quinn's Palace</title>
+  </head>
+  <body>
+
+    <p>This is the home page for Quinn's Palace</p>
+	
+	<p> <a href="page2.html">Link to the Dreamweaver page</a><br></br> </p>
+	
+	<p> <a href="table.html">table of goodies</a><br></br> </p>
+	
+	<p> <a href="SQLTable.php">SQL Table</a><br></br> </p>
+	
+	<p> <a href="page 4.html">fuck</a><br></br> </p>
+  </body>
+</html>
+
